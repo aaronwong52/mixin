2c065422bbfe1b24dc28f929ee2fcde437f62c80
DeliciousTrips <DeliciousTrips>
2023-01-16 12:17:37 -0800

    separated click and drag

---
+++ src/editor.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import * as Tone from 'tone';
+
+import { useState, useEffect } from 'react';
+
+const StyledEditor = styled.div`
+    width: 100vw;
+    height: 250px;
+    background-color: #fafaf7;
+`
+
+function Editor(recording) {
+    return (
+        <StyledEditor>
+
+        </StyledEditor>
+    )
+}
+
+export default Editor;
