13a77c96cf775a28454d4ad0719c0b2ff6f5e008
Aaron Wong <aaronwong52@gmail.com>
2023-02-08 11:18:35 -0500

    working recording setup

--- src/mainTransport.jsx
+++ src/mainTransport.jsx
@@ -3,15 +3,15 @@ import p5 from 'p5';
 
 import styled from 'styled-components';
 import { Timeline, Transport } from 'tone';
+import NewRecording from './newrecording';
 
 const TransportView = styled.div`
-  height: 500px;
+  height: 400px;
   width: 100vw;
   display: ${props => props.display ? "none" : "flex"};
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  margin-top: 50px;
 `;
 
 const TransportObjects = styled.div`
@@ -38,8 +38,25 @@ const PlayButton = styled.button`
   border: none;
   background: url('/images/play-button.png') no-repeat;
   background-size: 35px;
+  :hover {cursor: pointer;}
 `;
 
+const RecordingsView = styled.div`
+  width: 90vw;
+  height: 110px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 10px;
+`;
+
+const RecordingView = styled.div`
+    width: 100px;
+    height: 75px;
+    background-color: #d6b8f5;
+    border-radius: 10px;
+    margin-right: 15px;
+`;
 
 /* 
 
@@ -64,17 +81,25 @@ block swipe to go back
 
 */
 
-function MainTransport({display, recordings, playPosition, setPosition, play}) {
+function MainTransport({display, recordings, playPosition, setPlayPosition, play}) {
 
     let playBtn = document.getElementById("play_btn");
     const transport = useRef();
     const length = 2000;
 
+    const inflateRecordings = () => {
+      return recordings.map(r => (
+        <RecordingView className="recording_clip"></RecordingView>
+        // <NewRecording key={Math.random() * 1000}
+        //   newRecording={r} setPlayPosition={setPlayPosition}>
+        // </NewRecording>
+      ));
+    }	
+
     useEffect(() => {
       const s = (sketch) => {
         let x = length;
-        let y = 150;
-        let font;
+        let y = 30;
 
         sketch.setup = () => {
           sketch.createCanvas(x, y);
@@ -85,23 +110,31 @@ function MainTransport({display, recordings, playPosition, setPosition, play}) {
           sketch.fill(51)
           sketch.textSize(12);
           sketch.line(0, y-20, x, y-20); // baseline
-          let i=0;
-          while (i < x - 50) {
-            i = i + 50;
-            sketch.line(i, y-25, i, y-15);
-            sketch.text(x - (x - i + 50), i, y);
+          let i = 0;
+          while (i < x) {
+            sketch.line(i + 10, y-25, i + 10, y-15);
+            sketch.text(i, i+10, y);
             sketch.textAlign(sketch.CENTER);
+            i = i + 50;
           }
         };
       };
       let wavep5 = new p5(s, transport.current);
     }, []);
 
+    useEffect(() => {
+      let clips = document.getElementsByClassName("recording_clip");
+      for (let c=0; c < clips.length; c++) {
+        clips[c].style.width = (recordings[c].size / 150) + "px";
+      }
+      
+    }, [recordings]);
+
     return (
         <TransportView id="transportview" display={display}>
-          <TransportObjects>
-            {/* each object maps to a recording view, next implement waveform drawer! */}
-          </TransportObjects>
+          <RecordingsView>
+            {inflateRecordings()}
+          </RecordingsView>
           <TransportTimeline id="timeline" ref={transport}>
           </TransportTimeline>
             <PlayButton type="button" id="play_btn" onClick={play}></PlayButton>
